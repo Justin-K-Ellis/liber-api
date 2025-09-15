8202bf570bb147268eb9d087390bf4b32cb6a6e8
Justin-K-Ellis <jlke45@gmail.com>
2025-09-15 20:29:54 +0900

    feat: allow client to create a product
    Created a method in the Product model and corresponding route handler
    in the controller.

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -4,6 +4,39 @@ import Product from "../models/product.model.js";
 const productController = express.Router();
 const productModel = new Product();
 
+// == Create ==
+productController.post("/", async (req, res) => {
+  const { productId, productName, price } = req.body;
+  console.log(productId, productName, price);
+
+  if (
+    productId === undefined ||
+    productName === undefined ||
+    price === undefined
+  ) {
+    res.status(400);
+    return res.json({
+      message: "Product id, product name, or price invalid or missing.",
+    });
+  }
+
+  try {
+    const productIdNum = parseInt(productId);
+    const priceNum = parseInt(price);
+    await productModel.add(productIdNum, productName, priceNum);
+    res.status(201);
+    res.json({
+      message: "Product created.",
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({
+      message: "Something went wrong when creating this product.",
+    });
+  }
+});
+
+// == Read ==
 productController.get("/", async (_req, res) => {
   try {
     const products = await productModel.getAll();
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -3,6 +3,15 @@ import pool from "../../db/pool.js";
 class Product {
   constructor() {}
 
+  // == Create ==
+  async add(productId, productName, price) {
+    await pool.query(
+      "INSERT INTO product (product_id, product_name, price) VALUES ($1, $2, $3)",
+      [productId, productName, price]
+    );
+  }
+
+  // == Read ==
   async getAll() {
     const { rows } = await pool.query(
       "SELECT product_id, product_name, price FROM product"
